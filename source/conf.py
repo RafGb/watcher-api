89aed11e4947d9165cab708f457ee2e9b94da6a4
Rafis Gubaidullin <raf.gbd@gmail.com>
2018-03-19 12:31:55 +0500

    fix4

--- source/conf.py
+++ source/conf.py
@@ -31,14 +31,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc', 
-    'sphinxcontrib.httpdomain',
-    'sphinxcontrib.autohttp.flask',
-    'sphinxcontrib.autohttp.flaskqref',
-    'bodyschema',
-    'sphinxcontrib.restbuilder',
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
